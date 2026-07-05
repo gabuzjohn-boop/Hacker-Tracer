488168f53a9505ad8ff921271d4995ea4118b5d8
Admin <gabuz.john@gmail.com>
2026-07-05 20:56:38 +0300

    third push

---
+++ Hacker Tracer/CaesarCipher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker_Tracer
+{
+    internal class CaesarCipher
+    {
+        public static string Encrypt(string text, int shift)
+        {
+            shift = ((shift % 26) + 26) % 26;
+            var result = new System.Text.StringBuilder();
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    char baseChar = char.IsUpper(c) ? 'A' : 'a';
+                    result.Append((char)((c - baseChar + shift) % 26 + baseChar));
+                }
+                else result.Append(c);
+            }
+            return result.ToString();
+        }
+
+        public static string Decrypt(string text, int shift) => Encrypt(text, 26 - shift);
+        public static string EncryptForDisplay(string plainText, int shift) => Encrypt(plainText, shift);
+    }
+}
